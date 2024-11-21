e3a8eb45985884619552f52e760667742b172c3c
Litr0 <gonzalo.sobarzoa@gmail.com>
2024-11-21 16:04:09 +0100

    fix: update time quantiles for steam_2017_new dataset in get_data function

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -59,7 +59,7 @@ def get_data(dataset_name, different_new_nodes_between_val_and_test=False, rando
   if randomize_features:
     node_features = np.random.rand(node_features.shape[0], node_features.shape[1])
 
-  val_time, test_time = list(np.quantile(graph_df.ts, [0.35, 0.50]))
+  val_time, test_time = list(np.quantile(graph_df.ts, [0.45, 0.60]))
 
   """ # For steam_2017_new, we use different time splits
   if dataset_name == "steam_2017_new":
