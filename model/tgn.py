3b8cb81bdfcb3dabc1a62072f1dac984fa77fd1f
Litr0 <gonzalo.sobarzoa@gmail.com>
2024-12-02 11:52:38 +0100

    fix: add debug prints for memory update verification in TGN model

--- model/tgn.py
+++ model/tgn.py
@@ -162,6 +162,9 @@ class TGN(torch.nn.Module):
         # new messages for them)
         self.update_memory(positives, self.memory.messages)
 
+        print("memory[positives]:", memory[positives])
+        print("self.memory.get_memory(positives):", self.memory.get_memory(positives))
+
         assert torch.allclose(memory[positives], self.memory.get_memory(positives), atol=1e-3), \
           "Something wrong in how the memory was updated"
 
